4dfbab58db12686e952ce554d5e89a0c283b090a
mwon2928 <martin.kl.wong@gmail.com>
2015-08-27 11:18:56 +1000

    Generic script for checking parallel mode status of the machine and enables turning on and off the parallel computing pool.
    It can:
    - Open and close the parallel computing pool
    - Detect if the parallel computing toolbox is installed.
    - Detect version of matlab to determine whether to use parpool or matlabpool
    - Determine if the number of workers to be required are greater than the number of worker available on the machine.

--- parallelMode/parComp.m
+++ parallelMode/parComp.m
@@ -1,5 +1,19 @@
 function varargout = parComp(mode,numWorkers)
 
+%parComp Cross version toggle of parallel computer pools
+% this function can open and close a pool of workers for parallel computing
+% purposes. It checks matlab version and availability of the parallel
+% computing toolbox to determine if parallel computing is possible and
+% what function (parpool vs matlabpool) to use.
+%
+% status = parComp('open',numWorkers) attempts to open a pool. If the 
+% parallel computing toolbar isn't installed, parComp returns -1. Else
+% parComp attempts to open the number of workers specified by numWorkers. 
+% parComp will only open up to the maximum number of workers available in 
+% the pool.
+%
+% status = parComp('close') attempts to close an open pool.
+
 modeOpts = ['open '
             'close'];
         
@@ -8,26 +22,39 @@ if nargin == 1
 end
 
 switch lower(deblank(mode))
-    case lower(deblank(modeOpts(1,:)))
-        if exist('parpool','file') || exist('matlabpool','file')
-            clustInfo = parcluster('local');
-            maxWorkers = clustInfo.NumWorkers;
-            numWorkers = min([maxWorkers numWorkers]);
-            if exist('parpool','file')
+    case lower(deblank(modeOpts(1,:))) %Opening cluster
+        a = ver; %get matlab toolbox availability
+        parBoxExist =  ~isempty(intersect({a.Name},'Parallel Computing Toolbox'));
+        if parBoxExist
+            matVer = version('-release');
+            matVer = str2double(matVer(1:4));
+            if matVer>=2014
+                clustInfo = parcluster('local');
+                maxWorkers = clustInfo.NumWorkers;
+                numWorkers = min([maxWorkers numWorkers]); %Make sure required workers is not more than max workers
                 parObj = gcp('nocreate');
                 if isempty(parObj)
                     parpool(numWorkers);
+                else
+                    fprintf('Cluster already open\n')
                 end
-            elseif exist('matlabpool','file')
-                matlabpool('open',numWorkers);
-			end
-			varargout{1} = 0;
-        else
+                varargout{1} = 0;
+			else
+                clustInfo = findResource;
+                maxWorkers = clustInfo.ClusterSize;
+                numWorkers = min([maxWorkers numWorkers]); %Make sure required workers is not more than max workers
+                if matlabpool('size')==0
+                    matlabpool('open',numWorkers);
+                else
+                    fprintf('Cluster already open\n')
+                end
+                varargout{1} = 0;
+            end
+        else %No cluster
             warning('parComp:noParallelToolbox','This machine does not support parallel computing. Reverting to single core mode')
             varargout{1} = -1;
         end
-
-    case lower(deblank(modeOpts(2,:)))
+    case lower(deblank(modeOpts(2,:))) %Closing cluster
         if exist('parpool','file')
             parObj = gcp('nocreate');
             delete(parObj)
