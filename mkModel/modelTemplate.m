59d9a12d799368e4bd183b3a38bf4a7b06eeedf1
mwon2928 <martin.kl.wong@gmail.com>
2015-10-26 15:31:38 +1100

    Adding validation files. This covers most fundamental reactions as well as Michaelis Menten type enzyme kinetics.

---
+++ mkModel/modelTemplate.m
@@ -0,0 +1,162 @@
+%% Define postProc states
+% This template contains comments that guide the creation of a model. These
+% are marked by lines with a simple '%' at the front. These can be deleted
+% once you are familiar with how to create a model. Leave the lines with
+% '%%'.
+
+%% Forward: Parameters
+% A common feature in these model files is the idea of a "parameter". Any
+% variable that is a parameter is defined using six potential formats:
+%	1) [val]           : Known and fixed parameter
+%	2) [NaN]           : Unknown parameter, default range used
+%	3) [NaN lb ub]     : Unknown parameter, custom range
+%   5) [grp*i]         : Unknown parameter with other parameters with the same
+%                        value, custom range. Note the "i" is iota, i.e. imaginary.
+%   6) [factor + grp*i]: Unknown parameter that is a multiplicative factor of
+%                        another parameter. Parameter of this format is
+%                        dependent. Note the "i" is iota, i.e. imaginary.
+% grp is a positive integer. All parameters with grp that is the same
+% integer are in the same group.
+
+%% Compartment definition
+%
+% spcComp = {'Compartment name', relative size};
+%
+% ~~~Guide~~~
+% "Compartment name" is the name of the compartment in the system
+%
+% Relative size is the relative size of the compartment. Can be infinite
+% (in which case the concentration of species in that compartment is
+% fixed).
+%
+% ~~~Example~~~
+% We are modelling the a cell with only a cytoplasm (cyto) and plasma
+% membrane (PM). The extracellular space (ES) is considered far larger than 
+% either compartments which we consider infinite in size. This would give 
+% the following.
+%
+spcComp = {'Cyto', 1;
+           'PM'  , 0.05;
+           'ES'  , Inf;
+           };
+
+%% Model species definition
+%
+% modSpc ={'State name', 'Compatment'  , conc/param};
+%
+% ~~~Guide~~~
+% "State" is the name of the state to be included in the simulation
+%
+% Compartment name of the compartment as defined in the compartment part.
+%
+% Conc/param is either the concentration of the state. This is a parameter.
+%
+% ~~~Example~~~
+% We are modelling the protein AKT. It can either be cytosolic or plasma
+% membrane bound. It has two phosphorylation sites and can be dual
+% phosphorylated. AKT initially exists unphosphorylated and in the cytosol.
+% mTORC2 is also included to phosphorylation AKT.
+% We would define this by:
+%
+modSpc = {'AKT'     ,'Cyto', NaN;
+          'mAKT'    ,'PM'  , 0;
+          'p473mAKT','PM'  , 0;
+          'p473AKT' ,'Cyto', 0;
+          'mTORC2'  ,'PM'  , 1};
+
+%% Relationship between simulation state and model state association
+
+% dataSpc = {'Exp State Name',{'Sim State Name 1','Sim State Name 2'}};
+
+% ~~~Guide~~~
+%
+% Overview: The way this is designed is each experiment state is
+% "constructed" from the simulated states. Thus one experimental states
+% maps to multiple simulated states.
+%
+% Specifics:
+% "Exp Name" is the name of the state as labelled in the experimental
+% data.
+%
+% "Sim State Name 1" and "Sim State Name 2" are the state names as labelled
+% in the simulation. The states included are summed together.
+%
+% An asterix (*) at the front of simulation state names implies the
+% implicit complexes for that state (calculated by not created explicitly
+% in xMod, such as ES complexes) are included as well.
+%
+%
+% ~~~Example~~~
+% Phosphorylation of p473AKT is experimental state to be compared. The
+% experiment is a Western Blot without cell fractionation, hence plasma
+% membrane and cytosolic AKT are captured. In our model the two pools of
+% AKT are discrete, hence the group of simulation states corresponding to
+% the experimental state are p473AKT and p473mAKT. Additionally, enzyme
+% substrate complexes for both the simulation states are included, so they
+% need to be asterixed.
+%
+% The final row for xData is hence:
+ dataSpc = {'p473AKT',{'*p473AKT','*p473mAKT'}};
+
+%% Features of default parameters
+%
+% Bnd.(param) = [lb ub]
+% (param) is the parameter type
+%
+% ~~~Guide~~~
+% The above gives the default boundaries for each type of parameter. There
+% are currently five parameter types:
+%   k0   - Zeroth order synthesis type
+%   k1   - First order degradataion type
+%   k2   - Second order associaition type
+%   Km   - Association/Dissociation/Michaelis constant type
+%   Conc - Concentration type
+%
+Bnd.k0  = [1e01 1e04];
+Bnd.k1  = [5e-5 0.5];
+Bnd.k2   = [5e-5 5e-1];
+Bnd.Km   = [1e-2 1e02];
+Bnd.Conc = [1e-1 1e1];
+
+%% Reactions
+%
+% ~~~Guide~~~
+%
+% Reactions are stored in the variable rxn. Each reaction has
+% between 3 to 6 fields depending on the reaction to be created. (c) marks
+% fields that are compulsory. The fields are:
+%   - Label (c): Identifier of the reaction. Used for labelling outputs.
+%   - k     (c): Reaction rate. Is a parameter.
+%   - Sub      : List of substrates written as a cell array.
+%   - Prod     : List of products written as a cell array.
+%   - Enz      : Mediating enzyme
+%   - Km       : Michaelis Constant for enzymatic reaction. Is a parameter.
+%   
+% Begin a new reaction by placing (end+1) after rxn. Continue a reaction by
+% placing (end) after rxn [The reason for this is due to programming. Just
+% follow this rule and you will not run into trouble].
+
+% The combination used for each reaction determines the rules used in the
+% backend of the program. This will be explained more later.
+%
+% For example, with an unknown Km and a known reaction rate of 0.1mol/s,
+% the phosphorylation of AKT at the membrane by mTORC2 can be written as:
+
+rxn(end+1).label = 'mAKT -> p473mAKT | mTORC2';
+    rxn(end).sub = 'mAKT';  
+    rxn(end).prod= 'p473mAKT'; 
+    rxn(end).enz = 'mTORC2';
+	rxn(end).Km  = NaN; 
+    rxn(end).k   = 0.1; 
+
+ % More information on types of reactions:
+ % If the fields (excluding label and k) are:
+ %      - 1 Sub: Degradataion
+ %      - 1 Sub, 1 Enz: Enzyme mediated degradataion
+ %      - 1 Sub, 1 Prod : Transformation
+ %      - 1 Sub, 2+ Prod: dissociation
+ %      - 2 Sub, 1 Prod : Association 
+ %      - 1 Prod: Constant synthesis
+ %      - 1 Prod, 1 Enz: Enzyme mediated synthesis
+ %      - 1 Sub, 1 Enz, Prod: Enzymatic reaction that can produce any 
+ %        number of products
---
+++ validation/testCases.m
@@ -0,0 +1,84 @@
+%%%testCase%%%
+
+% This program tests whether the odeQSSA program is working correctly.
+
+%% Synthesis
+method = 'syn';
+
+x0 = [0 0 0 0 0 0 0];
+k = [1];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[~,~,YMod] = findTC(@SynProg,tReal,'p',1,'-b');
+
+resid(1) = sum(YReal(:,1)-YMod(:,1));
+
+%% Uni
+method = 'uni';
+%Dissociation
+x0 = [1 0 0 0 0 0 0];
+k = [1 1 1 0];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@UniDis,tReal,'p',1,'-b');
+resid(2) = sum(YReal(:,1)-YMod(:,1));
+
+%Interconversion
+x0 = [1 0 0 0 0 0 0];
+k = [1 0 1 0];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@UniCon,tReal,'p',1,'-b');
+resid(3) = sum(YReal(:,1)-YMod(:,1));
+
+%Degradation
+x0 = [1 0 0 0 0 0 0];
+k = [1 1 0 0];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@UniDeg,tReal,'p',1,'-b');
+resid(4) = sum(YReal(:,1)-YMod(:,1));
+
+%SynMM
+x0 = [1 0 0 0 0 0 0];
+k = [1 1 1 1];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@SynMM,tReal,'p',1,'-b');
+resid(5) = sum(YReal(:,2)-YMod(:,1));
+
+%% Bi
+method = 'bi';
+% Association
+x0 = [1 1 0 0 0 0 0];
+k = [1 1 0];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@BiAss,tReal,'p',1,'-b');
+resid(6) = sum(YReal(:,1)-YMod(:,1));
+
+% MM Enzyme Kinetics
+x0 = [1 1 0 0 0 0 0];
+k = [1 1 1];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@BiMM,tReal,'p',1,'-b');
+resid(7) = sum(YReal(:,1)-YMod(:,1));
+
+% MM Enzyme Kinetics Deg
+x0 = [1 1 0 0 0 0 0];
+k = [1 0 1];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@BiMMDeg,tReal,'p',1,'-b');
+resid(8) = sum(YReal(:,1)-YMod(:,1));
+
+%% Enzyme Kinetic
+method = 'enzQSSA';
+% Single
+x0 = [1 1 0 0 1 0 0];
+k = [1 1 1 0 1 1];
+kQSSA = [1 2 1 Inf];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 30],x0);
+[tMod,~,YMod] = findTC(@enzKinetic,tReal,'p',kQSSA,'-b');
+resid(9) = sum(YReal(end,1)-YMod(end,1));
+
+% Reversible
+x0 = [1 1 0 0 1 0 0];
+k = [1 1 1 1 1 1 ];
+kQSSA = [1 2 1 2];
+[tReal,YReal] = ode15s(@(t,x) testReactions(t,x,method,k),[0 10],x0);
+[tMod,~,YMod] = findTC(@enzKinetic,tReal,'p',kQSSA,'-b');
+resid(10) = sum(YReal(end,1)-YMod(end,1));
