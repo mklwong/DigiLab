e2fbe3bf337576498b4f612434686d184e17ebab
mwon2928 <martin.kl.wong@gmail.com>
2016-07-01 15:57:40 +1000

    Committing findTC support files.

---
+++ solvers/findTC/findTC/compDis.m
@@ -0,0 +1,33 @@
+function Y = compDis(model,Y)
+
+% Y2 = compDis(Y,G)
+x = model.modSpc;
+[~,kmInd] = ismember({model.param.name},'Km');
+kmInd = find(kmInd);
+G = model.param(kmInd).matVal;
+
+if ~isempty(G)
+	% Remove the terms which correlate with change in enzyme/substrate
+	% concentration
+	G(G(:,1)==G(:,2) | G(:,1)==G(:,3),:)=[];
+
+	% Column 1 now contains all the complex indexes. We use only column 2 to
+	% identify enzymes and substrates as column 3 is a duplicate. Column 4 is
+	% the Km's so we remove them.
+	G(:,[3 4]) = [];
+
+	% Sort G by sub/enz index
+	[~,I] = sort(G(:,2)); 
+	G = G(I,:);
+
+	M = eye(size(x,1));
+	scale = model.comp;
+	scale = scale(:,ones(1,length(scale)));
+	M((G(:,2)-1)*max(G(:,1))+G(:,1))=1;
+	Y = Y*(M.*scale);
+	Y = Y./(scale(:,ones(1,size(Y,1))))';
+	
+	% Remove complexes
+	rmIndx = unique(G(:,1));
+	Y(:,rmIndx) = [];
+end
