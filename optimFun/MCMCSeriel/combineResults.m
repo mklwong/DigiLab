ab5a91edab8d4a5a2217f0f3c4d26b63c02fc957
mwon2928 <martin.kl.wong@gmail.com>
2016-07-18 12:07:37 +1000

    Added combineResult function. This function combines result structures generated by MCMCSeriel.

---
+++ optimFun/MCMCSeriel/combineResults.m
@@ -0,0 +1,27 @@
+function allResult = combineResults(varargin)
+
+%combineResults merges all MCMC result structures (pts, logP and ptUn)
+%   allResult = combineResults(result1,result2,....) All arguments of the
+%   function are expected to be result structures. All result then combines
+%   these together into a single structure. This function has no other
+%   options or arguments.
+%
+%   A result structure has the following fields:
+%       result.pts  - a list of parameter sets where each row is a set
+%       result.logP - a list of corresponding -logP value for each
+%       parameter set
+%       result.ptUn - a list of logical flag that states whether the
+%       corresponding parameter set is unique
+%       result.T    - Tempering temperature of the result run
+%       result.model- Parameter set that produced the best fit
+
+allResult = varargin{1};
+
+for ii = 2:length(varargin)
+    tmpResult = varargin{2};
+    allResult.pts = [allResult.pts;tmpResult.pts];
+    allResult.logP = [allResult.logP;tmpResult.logP];
+    allResult.ptUn = [allResult.ptUn;tmpResult.ptUn];
+end
+
+allResult.best = allResult.pts(allResult.logP==min(allResult.logP),:);
