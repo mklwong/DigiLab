a48153a680d6e2a3ea5a01572e120265a0037b6a
mwon2928 <martin.kl.wong@gmail.com>
2016-07-21 13:05:03 +1000

    Type of change in runVar.priorP check (accidentally written as opts.runVar)

--- optimFun/MCMC.m
+++ optimFun/MCMC.m
@@ -278,7 +278,7 @@ printCheckpoint('1',outputName,opts.disp);
 % ----- Find new seed point ------
 %      --------------------
 if mod(stepCount,opts.resample)==0
-	if ~isempty(opts.runVar.priorP)
+	if ~isempty(runVar.priorP)
 		% Select new point from prior based on goodness of fit of the
 		% prior
 		rngPt = rand(1);
